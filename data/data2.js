8f8f4b15e7c93fec8e03f6bdcec6d3d8ee1409b3
Julia <>
2021-09-09 11:56:16 +0200

    first commit

---
+++ data/data2.js
@@ -0,0 +1,542 @@
+var data = [
+    {
+        "base": "Baozhong tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Four seasons baozhong tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Baozhong tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Boba, coconut jelly",
+        "drinkEN": "Four seasons baozhong + boba & coconut jelly",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Baozhong tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Boba",
+        "drinkEN": "Boba baozhong tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Baozhong tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pearl",
+        "drinkEN": "Pearl baozhong tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Baozhong tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pudding",
+        "drinkEN": "Pudding baozhong tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Baozhong tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Four seasons baozhong milk tea",
+        "categoryEN": "Milk tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Assam black tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Ice cream",
+        "drinkEN": "Ice cream black tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "Pineapple",
+        "texture": "No",
+        "drinkEN": "Pineapple black tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Boba",
+        "drinkEN": "Boba black tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Boba",
+        "drinkEN": "Boba milk tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pearl",
+        "drinkEN": "Pearl black tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pearl",
+        "drinkEN": "Pearl milk tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Coconut jelly",
+        "drinkEN": "Coconut jelly milk tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pudding",
+        "drinkEN": "Pudding milk tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pudding",
+        "drinkEN": "Pudding black tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Black tea macchiato",
+        "categoryEN": "Milk tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Black tea",
+        "base2": "Fresh Milk",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Black tea latte",
+        "categoryEN": "Fresh milk",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Cocoa",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Cocoa with milk powder",
+        "categoryEN": "Milk tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Cocoa",
+        "base2": "Fresh Milk",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Cocoa latte",
+        "categoryEN": "Fresh milk",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Jasmine green tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "Lemon",
+        "texture": "No",
+        "drinkEN": "Lemon green tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "Plum",
+        "texture": "No",
+        "drinkEN": "Plum green tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "Kumquat",
+        "texture": "No",
+        "drinkEN": "Kumquat green tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "Kumquat",
+        "texture": "No",
+        "drinkEN": "Kumquat plum green tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "Yakult",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Yakult green tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "Yuzu",
+        "texture": "No",
+        "drinkEN": "Yuzu green tea",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Boba",
+        "drinkEN": "Boba green tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Boba",
+        "drinkEN": "Boba green milk tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pearl",
+        "drinkEN": "Pearl green tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pearl",
+        "drinkEN": "Pearl green milk tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pudding",
+        "drinkEN": "pudding green milk tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pudding",
+        "drinkEN": "Pudding green tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Green milk tea",
+        "categoryEN": "Milk tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Green tea",
+        "base2": "Fresh Milk",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Green tea latte",
+        "categoryEN": "Fresh milk",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Lemon juice",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Lemon juice",
+        "categoryEN": "Fresh",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Lemonade",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "Kumquat",
+        "texture": "No",
+        "drinkEN": "Kumquat lemonade",
+        "categoryEN": "Fresh",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Milk tea",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Milk tea",
+        "categoryEN": "Milk tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Oolong",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Golden oolong",
+        "categoryEN": "Tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Oolong",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Boba",
+        "drinkEN": "Boba oolong tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Oolong",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Boba",
+        "drinkEN": "Boba oolong milk tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Oolong",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pearl",
+        "drinkEN": "Pearl oolong tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Oolong",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pearl",
+        "drinkEN": "Pearl oolong milk tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Oolong",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "Pudding",
+        "drinkEN": "Pudding oolong tea",
+        "categoryEN": "Texture",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Oolong",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Oolong machhiato",
+        "categoryEN": "Milk tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Oolong",
+        "base2": "Milk powder",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Golden oolong milk tea",
+        "categoryEN": "Milk tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Oolong",
+        "base2": "Fresh Milk",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Golden oolong latte",
+        "categoryEN": "Fresh milk",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Ovaltine",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Ovaltine",
+        "categoryEN": "Milk tea",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Ovaltine",
+        "base2": "Fresh Milk",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Ovaltine latte",
+        "categoryEN": "Fresh milk",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Plum juice",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "Lemon",
+        "texture": "No",
+        "drinkEN": "Lemon plum juice",
+        "categoryEN": "Fresh",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Plum tea",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "Kumquat",
+        "texture": "No",
+        "drinkEN": "Kumquat plum tea",
+        "categoryEN": "Fresh",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Yakult",
+        "base2": "Yakult",
+        "sugar": "No",
+        "fruit": "Lemon",
+        "texture": "No",
+        "drinkEN": "Lemon yakult",
+        "categoryEN": "Fresh",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Yakult",
+        "base2": "Yakult",
+        "sugar": "No",
+        "fruit": "Grapefruit",
+        "texture": "No",
+        "drinkEN": "Grapefruit yakult",
+        "categoryEN": "Fresh",
+        "storeEN": "50 arashi"
+    },
+    {
+        "base": "Yuzu juice",
+        "base2": "No",
+        "sugar": "No",
+        "fruit": "No",
+        "texture": "No",
+        "drinkEN": "Fresh yuzu juice",
+        "categoryEN": "Fresh",
+        "storeEN": "50 arashi"
+    }
+]
